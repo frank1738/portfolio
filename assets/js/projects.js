2f2b6800c9786ad3534e603baf52e26fe650244b
frank1738 <franklineosoro08@gmail.com>
2023-06-09 18:26:12 +0300

    update images

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -2,7 +2,8 @@ export const projects = [
   {
     id: 1,
     title: 'Admin',
-    imageURL: '../../assets/img/admin.png',
+    imageURL:
+      'https://www.thewechatagency.com/wp-content/uploads/2021/12/1_SwFB1o_k1LGprN-XRUZQ8w.jpeg',
     descripion:
       'This is an Admin Dashboard that provides administrators with an overview of various aspects of an E-commerce system. It contains three pages,four apps and Seven fully functional charts.',
     live: 'https://admin-dashboard-hpi4.onrender.com/',
@@ -11,7 +12,8 @@ export const projects = [
   {
     id: 2,
     title: 'Space Travelers Hub',
-    imageURL: '../../assets/img/space.webp',
+    imageURL:
+      'https://www.ripplesnigeria.com/wp-content/uploads/2019/06/Space-traveler.jpg',
     descripion:
       'Space Travelers Hub is a CRA, that allows space traverlers to explore missions and Rockets. Users can also join different space missions and also reserve rockets.',
 
@@ -22,7 +24,8 @@ export const projects = [
   {
     id: 3,
     title: 'Cryptocurrency Market',
-    imageURL: '../../assets/img/market.jpg',
+    imageURL:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFwSezuwn27-L8ycFWj9KguX7M_806nB_Sow&usqp=CAU',
     descripion:
       'This a web app that fetches data from CoinGecko API and displays the global cryptocurrency market cap and the changes in the last 24 hours',
 
@@ -32,7 +35,8 @@ export const projects = [
   {
     id: 4,
     title: 'FitClub',
-    imageURL: '../../assets/img/fit.png',
+    imageURL:
+      'https://img.freepik.com/free-photo/strong-man-training-gym_1303-23478.jpg?w=2000',
     descripion: 'This is a Gym website',
 
     live: 'https://gym-page-f507f.web.app/',
@@ -42,7 +46,8 @@ export const projects = [
   {
     id: 5,
     title: 'Coffee shop',
-    imageURL: '../../assets/img/coffee.jpeg',
+    imageURL:
+      'https://dailycoffeenews.com/wp-content/uploads/2018/10/coffee-801781_1280.jpg',
     descripion: 'This is a product landing page for a coffee shop',
 
     live: 'https://frank1738.github.io/coffee-shop-project-/',
