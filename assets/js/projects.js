eafae67735f32f1c6c5023e5e378def3e8a432f3
frank1738 <franklineosoro08@gmail.com>
2023-07-14 22:17:24 +0300

    update links

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -101,7 +101,7 @@ export const testimonials = [
   {
     name: 'Grace Muthui',
     imageUrl:
-      'https://pbs.twimg.com/profile_images/1654806859566465024/xWb_AKWq_400x400.jpg',
+      'https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg',
     title: 'Software Engineer',
     linkdin: 'https://www.linkedin.com/in/grayce-muthui/',
     content:
